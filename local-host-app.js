4bbe7ec115ca59ca7b489a82dd67d3568eafa2f1
Laura Unaeze <l.unaeze5@gmail.com>
2017-10-07 11:42:07 -0700

    Changed it so users receive random nice/rude messages when accessing server 1 or 2.

--- local-host-app.js
+++ local-host-app.js
@@ -7,12 +7,15 @@ var PORT2 = 7500;
 
 //The function hanlde/request for Port 1
 function handleRequest1 (request, response) {
-	response.end("You are very intelligent.");
+
+	var randomNice = ["You are very intelligent.", "You have a nice smile.", "You are a good cook.", "You are a loyal friend.", "You (usually) know when it's time to let go."];
+	response.end(randomNice[Math.floor(Math.random() * randomNice.length)]);
 }
 
 //The function handle/request for Port 2
 function handleRequest2 (request, response) {
-	response.end("You're really messy, and you always turn in your homework late.");
+	var randomRude = ["You smell bad.", "You are very messy.", "You always turn in your homework late.", "You are too intense.", "You can be really mean when you feel threatend or feel like you have to prove yourself."]
+	response.end(randomRude[Math.floor(Math.random() * randomRude.length)]);
 }
 
 //Use Node HTTP package to create server.
@@ -22,10 +25,10 @@ var server2 = http.createServer(handleRequest2);
 
 server1.listen(PORT1, function() {
 
-	console.log("Server listening on: http:localhost%s", PORT1)
+	console.log("Server listening on: http:localhost%s", PORT1);
 });
 
 server2.listen(PORT2, function() {
 
-	console.log("Server listening on: http:localhost%s", PORT2)
+	console.log("Server listening on: http:localhost%s", PORT2);
 });
